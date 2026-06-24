b44dc295737e2da1086aebe9b1475d9b1bc89eb5
wink <1043076764@qq.com>
2026-06-24 23:15:11 +0800

    Initial Commit

---
+++ TiaoxiuAlarm/TiaoxiuAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiaoxiuAlarmApp.swift
+//  TiaoxiuAlarm
+//
+//  Created by Wink on 2026/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TiaoxiuAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
